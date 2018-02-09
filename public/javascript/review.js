4975c405310a69e51685babc6bd9b104aec9855d
DorisCCLin <doriscclin@gmail.com>
2018-02-09 15:50:39 -0500

    js finished

--- public/javascript/review.js
+++ public/javascript/review.js
@@ -1,18 +1,28 @@
 
-console.log('test')
-var name = document.getElementById('name');
+
+var person = document.getElementById('person');
+var email = document.getElementById('email');
 var submit = document.getElementById('submit');
+var writer = document.getElementById('writer');
 var content = document.getElementById('content');
 var comments = document.getElementById('comments');
+var line = document.getElementById('line');
 
 submit.addEventListener('click', function(){
 	post()
 })
 
 function post(){
+	var title = document.createElement('p')
+	var auth= document.createTextNode(person.value)
+	title.appendChild(auth);
+    writer.appendChild(title);
 	var  para = document.createElement('p')
 	var  text= document.createTextNode(comments.value)
     para.appendChild(text);
     content.appendChild(para);
-    console.log(para)
+    var  br = document.createElement('br')
+    content.appendChild(br);
+    var  hr = document.createElement('hr')
+    line.appendChild(hr);
 }
