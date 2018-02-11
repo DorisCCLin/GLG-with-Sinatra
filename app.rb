aa8aaa94a034a20abdb8aa172bfc817c752b2b39
Ty Thompson <tylermoko@gmail.com>
2018-02-11 17:40:00 -0500

    email api and gallery adjusted

--- app.rb
+++ app.rb
@@ -23,21 +23,21 @@ get '/imgGallery' do
 	erb :imgGallery
 end
 
-get '/contacting' do
-	@email = params[:email]
-	@subject = params[:subject]
-	@content = params[:content]
+get '/contacting'  do
+    @email = params[:email]
+    @subject = params[:subject]
+    @content = params[:content]
     from = Email.new(email: @email)
-    to = Email.new(email: 'tylermoko@gmail.com')
+    to = Email.new(email: 'test@gmail.com')
     subject = @subject
     content = Content.new(type: 'text/plain', value: @content)
     mail = Mail.new(from, subject, to, content)
-    
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
     puts response.status_code
     puts response.body
     puts response.headers
-		erb :thank
-end
+     
+    erb :thank
+    end
 
